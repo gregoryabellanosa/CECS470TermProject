5f2c25edb2f9f86fe0fb3f9013a0487586618252
Gregory Abellanosa <gregoryabellanosa@gmail.com>
2018-05-15 20:57:52 -0700

    change styling of read more

--- js/project-desc.js
+++ js/project-desc.js
@@ -4,7 +4,7 @@ var character_limit = FindShortest();
 function FindShortest()
 {
 	for(var i = 1; i < text.length; i++)
-	{	
+	{
 		var shortest = text[0].children[3].innerText.length;
 		var prospect = text[i].children[3].innerText.length;
 		if(shortest > prospect)
@@ -20,13 +20,13 @@ function ReduceText()
 	var desc;
 	var link="";
 	for(var i = 0; i < text.length; i++)
-	{	
+	{
 		if(character_limit < text[i].children[3].innerText.length)
 		{
 			desc = text[i].children[3].innerText;
 			desc = desc.substring(0,character_limit);
 			text[i].children[3].innerText = desc;
-			link = "... <a href=\"projects.php#project" + i + "\">Read More</a>";  
+			link = "... <a class=\"readmore\" href=\"projects.php#project" + i + "\">Read More</a>";  
 			text[i].children[3].innerHTML += link;
 		}
 	}
